5084f731ed1a5634cb4de83327b9b9b44cb6fb21
Mohammad Nazari <m.nazari.dev@icloud.com>
2025-05-26 20:26:24 +0330

    sample send and receive data by rabbitmq protocol(AMQP)

--- Producer/Program.cs
+++ Producer/Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using RabbitMQ.Client;
+using System.Text;
 
-Console.WriteLine("Hello, World!");
+var factory = new ConnectionFactory { HostName = "localhost", Port = 5672, UserName = "admin", Password = "admin" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false,
+    arguments: null);
+
+const string message = "Hello World!";
+var body = Encoding.UTF8.GetBytes(message);
+
+await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "hello", body: body);
+Console.WriteLine($" [x] Sent {message}");
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();

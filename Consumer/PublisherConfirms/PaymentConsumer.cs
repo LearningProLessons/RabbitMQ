37e4673ebd338b434df53f8037c3f2ebde63316b
Mohammad Nazari <m.nazari.dev@icloud.com>
2025-06-10 12:14:47 +0330

    publisher confirms

---
+++ Consumer/PublisherConfirms/PaymentConsumer.cs
@@ -0,0 +1,40 @@
+﻿namespace Consumer.PublisherConfirms;
+
+using RabbitMQ.Client;
+using System.Text;
+using RabbitMQ.Client.Events;
+
+public class PaymentConsumer
+{
+    public static async Task RunAsync()
+    {
+        var factory = new ConnectionFactory
+        {
+            HostName = "localhost", Port = 5672, UserName = "admin", Password = "admin"
+        };
+
+        using var connection = await factory.CreateConnectionAsync();
+        using var channel = await connection.CreateChannelAsync();
+
+        await channel.QueueDeclareAsync("payment_gateway_confirm_queue", durable: true, exclusive: false,
+            autoDelete: false);
+        var consumer = new AsyncEventingBasicConsumer(channel);
+
+        consumer.ReceivedAsync += async (sender, ea) =>
+        {
+            var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+            var parts = message.Split(':');
+            var gateway = parts[0];
+            var amount = parts[1];
+
+            var trackingCode = Guid.NewGuid().ToString("N")[..6];
+            Console.WriteLine($"✔ پرداخت {amount} تومان از طریق {gateway} انجام شد | کد: {trackingCode}");
+
+            await channel.BasicAckAsync(ea.DeliveryTag, false);
+        };
+
+        await channel.BasicConsumeAsync("payment_gateway_confirm_queue", autoAck: false, consumer);
+        Console.WriteLine("🟢 Consumer آماده‌ست");
+        Console.ReadLine();
+    }
+}
---
+++ Producer/PublisherConfirms/PaymentPublisher.cs
@@ -0,0 +1,101 @@
+﻿namespace Producer.PublisherConfirms;
+
+using System.Text;
+using RabbitMQ.Client;
+
+
+public class PaymentPublisher : IAsyncDisposable
+{
+    private IConnection? _connection;
+    private IChannel? _channel;
+
+    private readonly LinkedList<ulong> _outstandingConfirms = new();
+    private readonly SemaphoreSlim _semaphore = new(1, 1);
+    private TaskCompletionSource<bool>? _allConfirmed;
+
+    public async Task<bool> PublishAsync(string gateway, int amount)
+    {
+        var factory = new ConnectionFactory
+        {
+            HostName = "localhost", Port = 5672, UserName = "admin", Password = "admin"
+        };
+
+        _connection = await factory.CreateConnectionAsync();
+
+        var channelOpts = new CreateChannelOptions(
+            publisherConfirmationsEnabled: true,
+            publisherConfirmationTrackingEnabled: true,
+            outstandingPublisherConfirmationsRateLimiter: new ThrottlingRateLimiter(2)
+        );
+
+        _channel = await _connection.CreateChannelAsync(channelOpts, CancellationToken.None);
+
+        await _channel.QueueDeclareAsync("payment_gateway_confirm_queue", durable: true, exclusive: false, autoDelete: false);
+
+        _allConfirmed = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+
+       
+        _channel.BasicAcksAsync += async (o, ea) =>
+        {
+            o = Task.Run(() => CleanOutstandingConfirms(ea.DeliveryTag, ea.Multiple));
+        };
+
+        _channel.BasicNacksAsync += async (o, ea) =>
+        {
+            o = Task.Run(() => CleanOutstandingConfirms(ea.DeliveryTag, ea.Multiple));
+        };
+
+        
+        var message = $"{gateway}:{amount}";
+        var body = Encoding.UTF8.GetBytes(message);
+
+        var seqNo = await _channel.GetNextPublishSequenceNumberAsync();
+        _outstandingConfirms.AddLast(seqNo);
+
+        await _channel.BasicPublishAsync(exchange: string.Empty, routingKey: "payment_gateway_confirm_queue", body: body);
+
+        using var timeout = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+        await using (timeout.Token.Register(() => _allConfirmed.TrySetResult(false)))
+        {
+            return await _allConfirmed.Task;
+        }
+    }
+
+    private async Task CleanOutstandingConfirms(ulong deliveryTag, bool multiple)
+    {
+        await _semaphore.WaitAsync();
+        try
+        {
+            if (multiple)
+            {
+                while (_outstandingConfirms.First is { } node && node.Value <= deliveryTag)
+                {
+                    _outstandingConfirms.RemoveFirst();
+                }
+            }
+            else
+            {
+                _outstandingConfirms.Remove(deliveryTag);
+            }
+
+            if (_outstandingConfirms.Count == 0)
+            {
+                _allConfirmed?.TrySetResult(true);
+            }
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (_channel is not null)
+            await _channel.CloseAsync();
+
+        if (_connection is not null)
+            await _connection.CloseAsync();
+    }
+}
+
